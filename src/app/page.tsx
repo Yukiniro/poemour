15396cdf6e47e5a7b51ae35a0c827065949d04bb
Yukiniro <yukiniro@hotmail.com>
2024-08-12 23:09:32 +0800

    feat: update github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import "@yukiniro/react-github-corners/dist/style.css";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <GithubCorners href="https://github.com/Yukiniro/next-starter" />
+      <GithubCorners href="https://github.com/Yukiniro/poemour" />
       <Main />
     </main>
   );
