b2cc7b1bf1df7e385c16de0edf4a9c08ff8777d9
Yukiniro <yukiniro@hotmail.com>
2024-08-15 20:09:04 +0800

    feat: add analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import GithubCorners from "@yukiniro/react-github-corners";
 import Main from "@/components/main";
 import "@yukiniro/react-github-corners/dist/style.css";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <GithubCorners href="https://github.com/Yukiniro/poemour" />
       <Main />
+      <Analytics />
     </main>
   );
 }
